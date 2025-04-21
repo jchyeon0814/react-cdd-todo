7dab56dde07a3fef47c06e22a0dc42fda9e54f02
jchyeon0814 <jchyeon0814@gmail.com>
2025-04-21 20:50:45 +0900

    타이틀 컴포넌트 추가(스토리북: 배경색 변경, 브라우저 라우터 적용

---
+++ src/components/atoms/AppTitle/index.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
+import { AppTitle } from '.';
+
+const meta = {
+    title: "atoms/AppTitle",
+    decorators: [
+        (Story) => (
+            <BrowserRouter>
+                <Story />
+            </BrowserRouter>
+        ),
+    ], 
+    parameters: {
+        backgrounds: {
+            default: 'Header background color',
+            values: [{ name: 'Header background color', value: '#304ffe' }],
+        },
+    },
+    component: AppTitle,
+    args: {
+
+    }
+} satisfies Meta<typeof AppTitle>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+
+    }
+};
